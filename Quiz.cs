24a58644d896257ecf2a22ddbd96fa2186ba9a8c
Mike <mike.sierra.7950@gmail.com>
2021-07-10 17:29:31 -0400

    FINAL

--- Quiz.cs
+++ Quiz.cs
@@ -8,21 +8,35 @@ namespace Chapter21_Studio
 {
     public class Quiz
     {
+        public Question num1 = new MultipleChoice("This is my first question.", "two", new string[] { "one", "two", "three", "four" });
+        public Question num2 = new MultipleChoice("This is my second question.", "one", new string[] { "one", "two", "three", "four" });
+        public Question num3 = new MultipleChoice("This is my third question.", "two", new string[] { "one", "two", "three", "four" });
+        public Question num4 = new MultipleChoice("This is my fourth question.", "three", new string[] { "one", "two", "three", "four" });
+        public Question num5 = new MultipleChoice("This is my fifth question.", "two", new string[] { "one", "two", "three", "four" });
+        public Question num6 = new MultipleChoice("This is my sixth question.", "one", new string[] { "one", "two", "three", "four" });
+        public Question num7 = new MultipleChoice("This is my seventh question.", "four", new string[] { "one", "two", "three", "four" });
+        public Question num8 = new CheckBox("This is my eight question.", "one", new string[] { "one", "two", "three", "four" });
+        public Question num9 = new CheckBox("This is my nine question.", "one,two,three,four", new string[] { "one", "two", "three", "four" });
+        public Question num10 = new CheckBox("This is my tenth question.", "two,three,four", new string[] { "one", "two", "three", "four" });
+        public Question num11 = new TrueFalse("This is my eleventh question.", "true");
+        public Question num12 = new TrueFalse("This is my twelfth question.", "false");
+        public Question num13 = new TrueFalse("This is my thirteenth question.", "true");
+        public Question num14 = new TrueFalse("This is my fourteenth question.", "false");
+        public Question num15 = new TrueFalse("This is my fifteenth question.", "false");
 
         public static List<Question> Questions = new List<Question>();
         public static void Add(Question question)
         {
             Questions.Add(question);
         }
-
-        public static void StartQuiz()
+        public void StartQuiz()
         {
             foreach (Question question in Questions)
             {
                 question.PrintQuestion();
             }
         }
-        public static void GradeQuiz()
+        public void GradeQuiz()
         {
             double score =100* (Question.NumCorrect / Question.TotalPossPoints);
             double finalScore = Math.Round(score);
